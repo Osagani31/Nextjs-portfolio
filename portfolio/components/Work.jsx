01685d637a1364fd87fd3e17aee83f64cd1fe59b
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-02-13 11:04:02 +0530

    Refactor code structure for improved readability and maintainability

--- portfolio/components/Work.jsx
+++ portfolio/components/Work.jsx
@@ -30,7 +30,7 @@ const Work = () => {
         
       </div>
     <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] 
-    border-gray-700 rounded-full py-3 px-10 mx-auto my-20 transition-colors duration-500 hover:bg-gray-300'>
+    border-gray-700 rounded-full py-3 px-10 mx-auto my-20 transition-colors duration-500 hover:bg-gray-300 font font-ovo'>
         Show more
         <img src={assets.right_arrow_bold} alt='Right arrow' className='w-4'/>
     </a>
